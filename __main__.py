5d01da2c5847470d769149ae3d6411ca12b0dd4a
joelaniado <jlaniado.g@gmail.com>
2022-05-13 14:15:44 -0500

    Base image compression algorithm using Kmeans is done. only missing input features and deployment.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,13 @@
 from src.data_import import import_image
+from src.model import image_compression
 
 
 def main():
-    import_image('images/josh.jpg')
+    seed = 24
+    k = 250
+    file = 'josh.jpg'
+    im, og_shape = import_image('images/input/{}'.format(file))
+    image_compression(seed, k, im, og_shape, file)
 
 
 if __name__ == "__main__":
---
+++ src/model.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+def get_comp_image(cen, lab):
+    new_image = [cen[i] for i in lab ]
+    return new_image
+
+
+def image_compression(seed, k, image, shape, file):
+    k_means = KMeans(n_clusters=k,random_state=seed).fit(image)
+    centroids, labels = k_means.cluster_centers_, k_means.labels_
+    vec_comp_im = np.array(get_comp_image(centroids, labels))
+    print(vec_comp_im, vec_comp_im.shape)
+    compressed_im = np.reshape(vec_comp_im,shape)
+    cv2.imwrite('images/output/{}'.format(file), compressed_im)
+    plt.imshow((compressed_im/255)[...,::-1])
+    plt.show()
+    return compressed_im
+
+
+
