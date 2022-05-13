b66403b34e49c99ae15b47cb044e24c3e08de2a1
joelaniado <jlaniado.g@gmail.com>
2022-05-13 14:48:02 -0500

    Reformating of code

--- __main__.py
+++ __main__.py
@@ -5,8 +5,8 @@ from src.model import image_compression
 def main():
     seed = 24
     k = 250
-    file = 'josh.jpg'
-    im, og_shape = import_image('images/input/{}'.format(file))
+    file = "josh.jpg"
+    im, og_shape = import_image("images/input/{}".format(file))
     image_compression(seed, k, im, og_shape, file)
 
 
