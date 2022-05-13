dc37081e724a14c99be4662c2683271ceb6669ea
joelaniado <jlaniado.g@gmail.com>
2022-05-13 17:35:11 -0500

    Added streamlit aplication interface as well as reestructuring the app ready for deployment.

--- src/data_import.py
+++ src/data_import.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 
 
-def import_image(image):
-    im = cv2.imread(image)
-    print(im.shape)
+def import_image(file):
+    bytes_data = np.asarray(bytearray(file.read()), dtype=np.uint8)
+    im = cv2.imdecode(bytes_data, cv2.IMREAD_COLOR)
+    print('Original image shape: {}'.format(im.shape))
 
     proc_im = np.reshape(im, (im.shape[0] * im.shape[1], im.shape[2]))
-    print(proc_im.shape)
+    print('Processed image shape: {}'.format(proc_im.shape))
     return proc_im, im.shape
