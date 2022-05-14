ee57d4e040c29eebbb3acf5b15c9bdb64a28e93f
joelaniado <jlaniado.g@gmail.com>
2022-05-14 01:23:08 -0500

    stable final version of webapp, ready for deployment.

--- src/web_app.py
+++ src/web_app.py
@@ -7,31 +7,26 @@ import cv2
 import os
 
 
-
 class webapp:
     def buildapp(self):
-        st.title(
-            "Welcome to my image compression app."
-        )
-        st.subheader('Please begin by reading the project description on the sidebar.')
+        st.title("Welcome to my image compression app.")
+        st.subheader("Please begin by reading the project description on the sidebar.")
         st.sidebar.title("Project description")
-        st.sidebar.write(
-            "This project consists of using machine learning for compressing the size of an image by means "
-            "of clustering similar pixel color values. \n"
+        st.sidebar.subheader(
+            "Note: JPG images not supported, please use PNG extension."
         )
         st.sidebar.write(
             "Images are made out of individual pixels which are represented with a numeric value to specify"
-            "its specific color. To give some perspective, each pixel has the option to choose between 16,581,375 "
-            "different color values when rendering an image. Many of those colors are small variations of the same one "
-            "but slightly different. By uploading your image on the right, you can see just how many unique colors there"
-            " are in your picture. "
+            " its color. To give some perspective, each pixel has the option to choose between 16,581,375 "
+            "different color values when rendering an image. Many of those colors are small variations of each other so "
+            "by combining similar colors into one, we can reduce the amount of memory our picture needs."
         )
-
-        st.sidebar.write('In order to reduce the memory requirements of our image we can group similar colors of pixels'
-                         ' into a single color! This will help us reduce its size while keeping the same dimensions. ')
-        st.sidebar.write('Try it out! Select the number of colors that you want your image to have and see if you if the '
-                         'quality is comparable to the original.')
-        uploaded_file = st.file_uploader("Please upload your image here.")
+        st.sidebar.write(
+            "Try it out! Upload an image, select the number of colors to compress to and see the result!"
+        )
+        uploaded_file = st.file_uploader(
+            "Please upload your image here.", type=["png", "PNG"]
+        )
         if uploaded_file is not None:
             image, shape, color_n = import_image(uploaded_file)
             st.image(uploaded_file, "Image to be compressed.")
@@ -42,19 +37,20 @@ class webapp:
                     str(int(uploaded_file.size / 1024)),
                 )
             )
-            st.sidebar.write(
-                'In order to reduce the memory requirements of our image we can group similar colors of pixels'
-                ' into a single color! This will help us reduce its size while keeping the same dimensions. ')
-            st.sidebar.write(
-                'Try it out! Select the number of colors that you want your image to have and see if you if the '
-                'quality is comparable to the original.')
             k = st.sidebar.slider(
-                "Select number of clusters:", min_value=2, max_value=255, value=30
+                "Colors selector:", min_value=2, max_value=150, value=30
             )
 
             if st.button("Compress"):
                 st.write(
-                    "Please wait, your image is being compressed....................................................")
+                    "Please wait, if the image is too big it might take a couple of minutes... "
+                )
+                st.write(
+                    "your image is being compressed...................................................."
+                )
+                st.write(
+                    'Meanwhile think about how to finish this sentence:"If you give a moose a cupcake......"'
+                )
 
                 compressed_im = image_compression(24, k, image, shape).astype(np.uint8)
                 st.image(
@@ -69,14 +65,15 @@ class webapp:
                             "Your image has {0} unique colors in {1} pixels. Its size is around {2} Kilobytes.".format(
                                 str(k),
                                 str(shape[0] * shape[1]),
-                                str(int(int(os.path.getsize(file.name))/1024)),
+                                str(int(int(os.path.getsize(file.name)) / 1024)),
                             )
                         )
                         st.download_button(
                             label="Download compressed image.",
                             data=file,
-                            file_name="compressed_image.png",
+                            file_name="compressed_{}".format(uploaded_file.name),
                         )
                 os.remove("images/output/temp.png")
+
 
 #
